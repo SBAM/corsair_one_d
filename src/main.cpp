5b1df38bad71b8503623cda2387dfa4eccb99e35
Philippe Serreault <philippe.serreault@gmail.com>
2020-05-03 23:30:17 +0200

    Improve logs upon init failure.

--- src/main.cpp
+++ src/main.cpp
@@ -16,14 +16,29 @@ int main()
     cod::devices_wrapper devs(lusb_ctx);
     auto desc = devs.get_desc(cod::defs::vendor_id, cod::defs::product_id);
     if (desc == std::nullopt)
+    {
+      spdlog::warn("main() failed to grab description for device with "
+                   "vendor_id={0:#x} product_id={0:#x}",
+                   cod::defs::vendor_id, cod::defs::product_id);
       return EXIT_PREVENT_RESTART;
+    }
     auto dev_hdl = devs.get_dev(cod::defs::vendor_id, cod::defs::product_id);
     if (!dev_hdl)
+    {
+      spdlog::warn("main() failed to get USB device handler");
       return EXIT_PREVENT_RESTART;
+    }
     if (!cod::check_device_id<cod::defs>(dev_hdl))
+    {
+      spdlog::warn("main() device handle doesn't match device_id={0:#x}",
+                   cod::defs::device_id);
       return EXIT_PREVENT_RESTART;
+    }
     if (!cod::check_has_temp_sensor<cod::defs>(dev_hdl))
+    {
+      spdlog::warn("main() device has no temperature sensor");
       return EXIT_PREVENT_RESTART;
+    }
     // if anything above went wrong, daemon shouldn't be restarted
     cod::daemon codd(sw, dev_hdl, 60, 40, 2s, 10s);
     codd.start();
@@ -36,16 +51,13 @@ int main()
     {
       // stop may convey an exception thrown in daemon thread, restart
       spdlog::error("daemon::stop() exception=[{}]", e.what());
-      spdlog::apply_all([&](auto logger) { logger->flush(); });
       return EXIT_RESTART;
     }
   }
   catch (const std::exception& e)
   {
     spdlog::error("main() exception=[{}]", e.what());
-    spdlog::apply_all([&](auto logger) { logger->flush(); });
     return EXIT_PREVENT_RESTART;
   }
-  spdlog::apply_all([&](auto logger) { logger->flush(); });
   return EXIT_SUCCESS;
 }
